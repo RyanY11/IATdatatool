278b37fb1c667ece51e754f7e783f18c190a55cc
RyanY11 <36234986+RyanY11@users.noreply.github.com>
2023-07-18 19:35:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-IATdatatool
-V3.0
+Created on Tue Jul 18 19:30:59 2023
+
+@author: ryan
+
+网页版本的IAT数据处理工具，挂载在streamlit上对外使用
+
+使用逻辑：
+1. 下载模板
+2. 上传数据表
+3. 校验数据表
+4. 展示数据表概述
+5. 输入分析条件和参数
+6. 核心计算
+7. 可视化展示分析结果
+8. 下载结果表格
 
 """
 
 import streamlit as st
-import openai
+import pandas as pd
+import os
 
-# # 调用openai反馈一些好玩的话语
-# openai.api_key = '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'
+# 数据模板下载
+# @st.cache
+# def convert_df(dataframe):
+#     return dataframe.to_csv().encode('utf-8')
 
-# a = openai.Completion.create(
-#   model="text-davinci-003",
-#   prompt="给我说一句和心理学相关的鼓励的话吧",
-#   top_p=1,
-#   temperature=0,
-#   max_tokens=1000
-# )
-# # print(a['choices'][0]['text'])
-# st.info(a['choices'][0]['text'])
+local_path = os.getcwd()
+data_model = pd.read_csv(os.path.join(local_path, 'data_sample.csv'))
 
 # 页面绘制
 st.title('IAT数据处理工具')
-st.info('这是一个在线的，用于处理IAT实验数据的程序')
+st.info('从这里开始一些测试')
 st.write(' ')
-
+st.header('Step 1. 下载数据模板')
+st.info('点击下载数据模板，并按照模板填写数据表')
+st.download_button('下载数据模板',data=data_model,file_name='data_sample.csv',mime='text/csv')
 
 st.balloons()

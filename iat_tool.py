91ad7da51ef544f9000651a0af5e0ee638485be6
RyanY11 <36234986+RyanY11@users.noreply.github.com>
2023-11-07 11:20:29 +0800

    Update iat_tool.py

--- iat_tool.py
+++ iat_tool.py
@@ -69,7 +69,7 @@ def data_overview(dataframe):
     parts = dataframe['Participant'].nunique()
     types = ', '.join(dataframe['Running'].unique().tolist())
     
-    return (rows, parts, types)
+    return rows, parts, types
 
 
 # 总体-过快/过慢剔除
